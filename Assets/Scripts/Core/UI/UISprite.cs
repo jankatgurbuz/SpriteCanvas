cf7e287c0e28e0cfcae847803c18b0681b878e38
jankatgurbuz <jankatgurbuz@gmail.com>
2024-05-28 18:20:09 +0300

    fix child-parent relation

--- Assets/Scripts/Core/UI/UISprite.cs
+++ Assets/Scripts/Core/UI/UISprite.cs
@@ -5,6 +5,7 @@ namespace Core.UI
     public class UISprite : UIElement
     {
         [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Vector3 _spriteSize;
 
         public override void ArrangeLayers(string sortingLayer, int sortingOrder)
         {
@@ -16,6 +17,8 @@ namespace Core.UI
             Vector3 viewportCenterPosition, float balance)
         {
             var spriteSize = _spriteRenderer.sprite.bounds.size;
+            _spriteRenderer.size = _spriteSize;
+            
             if (_referenceSprite == null)
             {
                 _responsiveOperation.AdjustUI(screenHeight, screenWidth, spriteSize, _itemPosition,
@@ -23,14 +26,23 @@ namespace Core.UI
             }
             else
             {
-                var size = _referenceSprite.sprite.bounds.size;
+                var referenceSpriteSize = _referenceSprite.sprite.bounds.size;
+                var referenceTransform = _referenceSprite.transform;
+                var globalReferenceSize = GetGlobalSize(referenceSpriteSize, referenceTransform);
 
                 _responsiveOperation.AdjustUI(
-                    size.y * _referenceSprite.transform.localScale.y,
-                    size.x * _referenceSprite.transform.localScale.x,
-                    spriteSize, _itemPosition,
+                    globalReferenceSize.y,
+                    globalReferenceSize.x,
+                    spriteSize,
+                    _itemPosition,
                     _referenceSprite.transform.position, balance);
             }
+        }
+
+        private Vector3 GetGlobalSize(Vector3 localSize, Transform myTransform)
+        {
+            var globalScale = myTransform.lossyScale;
+            return new Vector3(localSize.x * globalScale.x, localSize.y * globalScale.y, localSize.z * globalScale.z);
         }
     }
 }
---
+++ Assets/Scripts/DemoGame/DemoGameBeads.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace DemoGame
+{
+    public class DemoGameBeads : MonoBehaviour
+    {
+        [SerializeField] private DemoGameBeadsProp[] _demoGameBeadsProp;
+        private Dictionary<DemoGameBeadsColor, GameObject> _objects;
+        private Dictionary<DemoGameBeadsColor, GameObject> Objects => _objects;
+
+        private void Awake()
+        {
+            _objects = new Dictionary<DemoGameBeadsColor, GameObject>();
+            foreach (var item in _demoGameBeadsProp)
+            {
+                var obj = Create(item);
+                _objects.Add(item.Key, obj);
+            }
+        }
+
+        private GameObject Create(DemoGameBeadsProp prop)
+        {
+            var obj = new GameObject(prop.Sprite.name);
+            var spriteRenderer = obj.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = prop.Sprite;
+            obj.SetActive(false);
+            return obj;
+        }
+
+        public (GameObject, DemoGameBeadsColor) GetRandomObject()
+        {
+            var values = new List<DemoGameBeadsColor>(_objects.Keys);
+            var randomIndex = Random.Range(0, values.Count);
+            return (_objects[values[randomIndex]], values[randomIndex]);
+        }
+
+        [System.Serializable]
+        public class DemoGameBeadsProp
+        {
+            public DemoGameBeadsColor Key;
+            public Sprite Sprite;
+        }
+
+        public enum DemoGameBeadsColor
+        {
+            Blue,
+            Green,
+            Orange,
+            Purple,
+            Red,
+            Yellow,
+            Empty
+        }
+    }
+}
