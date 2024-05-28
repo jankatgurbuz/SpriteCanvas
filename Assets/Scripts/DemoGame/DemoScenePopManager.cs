50e0795429bd9c98681b9d3f223782425d47f283
jankatgurbuz <jankatgurbuz@gmail.com>
2024-05-28 20:00:25 +0300

    add a simple demo game

---
+++ Assets/Scripts/DemoGame/DemoScenePopManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace DemoGame
+{
+    public class DemoScenePopManager : MonoBehaviour
+    {
+        
+    }
+}
--- Assets/Scripts/Manager/SpriteCanvasManager.cs
+++ Assets/Scripts/Manager/SpriteCanvasManager.cs
@@ -1,4 +1,3 @@
-
 using System.Collections.Generic;
 using UnityEngine;
 using Core.UI;
@@ -9,15 +8,16 @@ namespace Manager
     {
         public static SpriteCanvasManager Instance;
         private Dictionary<string, SpriteCanvas> _spriteCanvasMap;
+        private Dictionary<string, UIElement> _targetUI;
 
         private void Awake()
         {
             Instance = this;
         }
 
-        public void Register(string str,SpriteCanvas spriteCanvas)
+        public void Register(string str, SpriteCanvas spriteCanvas)
         {
-            _spriteCanvasMap ??= new Dictionary<string,SpriteCanvas>();
+            _spriteCanvasMap ??= new Dictionary<string, SpriteCanvas>();
 
             if (!_spriteCanvasMap.TryAdd(str, spriteCanvas))
             {
@@ -33,6 +33,28 @@ namespace Manager
             }
 
             Debug.LogError($"No SpriteCanvas found with key {key}.");
+            return null;
+        }
+
+        public void RegisterTarget(string targetKey, UIElement uiElement)
+        {
+            _targetUI ??= new Dictionary<string, UIElement>();
+            
+            if (targetKey == string.Empty) return;
+            if (_targetUI.TryAdd(targetKey, uiElement)) return;
+            
+            Debug.Log(targetKey);
+            Debug.LogWarning("The targetKey already exists in the dictionary.");
+        }
+
+        public UIElement GetTarget(string targetKey)
+        {
+            if (_targetUI.TryGetValue(targetKey, out UIElement uiElement))
+            {
+                return uiElement;
+            }
+
+            Debug.LogWarning("The targetKey does not exist in the dictionary.");
             return null;
         }
     }
