9daab1393dd0a6286e96a40cf7f45a4d1d41dd52
jankatgurbuz <jankatgurbuz@gmail.com>
2024-06-06 17:32:16 +0300

    add sync alpha drawer attribute

---
+++ Assets/Scripts/SC/Editor/Drawer/SyncAlphaDrawer.cs
@@ -0,0 +1,43 @@
+using SC.Core.SpriteCanvasAttribute;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace SC.Editor.Drawer
+{
+    [CustomPropertyDrawer(typeof(SyncAlphaAttribute))]
+    public class SyncAlphaDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+
+            var alphaAttribute = (SyncAlphaAttribute)attribute;
+            if (!alphaAttribute.RunTimeSync)
+            {
+                if (Application.isPlaying)
+                    return;
+            }
+            
+            var target = property.serializedObject.targetObject as MonoBehaviour;
+            if (target == null) return;
+
+            if (target.TryGetComponent<SpriteRenderer>(out var sr))
+            {
+                if (property.propertyType == SerializedPropertyType.Float)
+                {
+                    property.floatValue = sr.color.a;
+                }
+            }
+            else if (target.TryGetComponent<TextMeshPro>(out var tmp))
+            {
+                if (property.propertyType == SerializedPropertyType.Float)
+                {
+                    property.floatValue = tmp.color.a;
+                }
+            }
+        }
+    }
+}
