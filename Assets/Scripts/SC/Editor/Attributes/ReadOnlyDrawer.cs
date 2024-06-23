9152087c934b7a17b0c2575b46725b99e6f1df24
Jankat <>
2024-06-23 10:07:02 +0300

    Add ReadOnlyAttribute

---
+++ Assets/Scripts/SC/Editor/Attributes/ReadOnlyDrawer.cs
@@ -0,0 +1,17 @@
+using SC.Core.SpriteCanvasAttribute;
+using UnityEditor;
+using UnityEngine;
+
+namespace SC.Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+}
